92f4b96d158f505b0ea9c02ab36e7ea6453e04ba
Michael Scholten <michael-scholten@hotmail.nl>
2023-11-22 12:53:36 +0100

    Useless columns are now automatically removed

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ else:
     path = input("Enter path to dataset: ")
 
 dataset = pandas.read_csv(path)
-print(dataset)
+for label in dataset.columns:
+    if dataset[label].min() == dataset[label].max():
+        del dataset[label]
 dataset = dataset / dataset.abs().max()
 print(dataset[:5])
 print(dataset.min())
 print(dataset.max())
+print(dataset.mean())
